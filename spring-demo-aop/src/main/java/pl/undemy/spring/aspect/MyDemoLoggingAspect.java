eaea30c531f357efe57bf3eaab171da2456190c8
IwantByJavaDev <misio_b@wp.pl>
2018-11-24 15:36:26 +0100

    declare pointcut for any method name addAccount only specific class

--- spring-demo-aop/src/main/java/pl/undemy/spring/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop/src/main/java/pl/undemy/spring/aspect/MyDemoLoggingAspect.java
@@ -12,7 +12,8 @@ public class MyDemoLoggingAspect {
 	
 	// let's start with an @Before advice
 	
-	@Before("execution (public void addAccount())") //declare pointcut for any method name addAccount
+//	@Before("execution (public void addAccount())") //declare pointcut for any method name addAccount
+	@Before("execution (public void pl.undemy.spring.dao.AccountDao.addAccount())") //declare pointcut for any method name addAccount only specific class
 	public void beforeAddAccountAdvice() {
 		System.out.println("\n=====>>>>> Executing @Before advice an addAccount()");
 	}
