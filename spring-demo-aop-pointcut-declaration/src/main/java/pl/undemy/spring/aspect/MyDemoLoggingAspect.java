0afd6f54684cbfcca9355018ae5e97ba38626171
IwantByJavaDev <misio_b@wp.pl>
2018-11-27 21:52:03 +0100

    Reusing pointcuts

--- spring-demo-aop-pointcut-declaration/src/main/java/pl/undemy/spring/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop-pointcut-declaration/src/main/java/pl/undemy/spring/aspect/MyDemoLoggingAspect.java
@@ -15,7 +15,13 @@ public class MyDemoLoggingAspect {
 	
 	@Before("forDaoPackage()") 
 	public void beforeAddAccountAdvice() {
-		System.out.println("\n=====>>>>> Executing @Before advice on method");
+		System.out.println("\n =====>>>>> Executing @Before advice on method");
 
+	}
+	
+	@Before("forDaoPackage()")
+	public void performApiAnalytics() {
+		System.out.println("\n =====>>>>> Executing @Before advice performApiAnalytics");
+		
 	}
 }
