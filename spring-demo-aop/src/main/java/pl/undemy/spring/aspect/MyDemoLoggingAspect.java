34c384e57929249a02cd5427e820f16d226700a5
IwantByJavaDev <misio_b@wp.pl>
2018-11-27 21:32:06 +0100

    clear comment

--- spring-demo-aop/src/main/java/pl/undemy/spring/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop/src/main/java/pl/undemy/spring/aspect/MyDemoLoggingAspect.java
@@ -8,16 +8,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyDemoLoggingAspect {
 
-	//this is where we add all of our related advices for logging
-	
-	// let's start with an @Before advice
-	
-//	@Before("execution (public void addAccount())") //declare pointcut for any method name addAccount
-//	@Before("execution (public void pl.undemy.spring.dao.AccountDao.addAccount())") //declare pointcut for any method name addAccount only specific class
-//	@Before("execution (public void add*())") //declare pointcut for any method name stat add I using wildcards
-//	@Before("execution (* add*())") //declare pointcut for any method name start "add" I using wildcards. Modifier doesn't matter
-//	@Before("execution (* addAccount(..))") //declare pointcut for any parameter
-	//@Before("execution (* pl.undemy.spring.dao.*.*(pl.undemy.spring.main.Account))") //declare pointcut for parameter from current package
+
 	@Before("execution (* pl.undemy.spring.dao.*.*(pl.undemy.spring.main.Account, ..))") //declare pointcut for parameter from current package, and more parameter any type
 	public void beforeAddAccountAdvice() {
 		System.out.println("\n=====>>>>> Executing @Before advice on method");
